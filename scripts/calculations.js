8cca938a058cf653f5a9832e7819517fe5cf490d
santi <santiagoespitic@gmail.com>
2026-06-03 18:54:31 -0500

    feat: add category expenses calculation

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,2 +1,48 @@
 let budgetValue = 0;
 let totalExpensesValue = 0;
+const expenseEntries = [
+  ["groceries", 33],
+  ["restaurants", 50],
+  ["transport", 12],
+  ["home", 70],
+  ["subscriptions", 14],
+  ["groceries", 28],
+  ["subscriptions", 12],
+];
+
+for (const entry of expenseEntries) {
+  totalExpensesValue += entry[1];
+}
+
+function calculateAverageExpense() {
+  if (expenseEntries.length === 0) {
+    return 0;
+  }
+  return totalExpensesValue / expenseEntries.length;
+}
+
+function calculateBalance() {
+  return budgetValue - totalExpensesValue;
+}
+
+let balanceColor = "green";
+
+function updateBalanceColor() {
+  if (calculateBalance() < 0) {
+    balanceColor = "red";
+  } else if (calculateBalance() < budgetValue * 0.25) {
+    balanceColor = "orange";
+  } else {
+    balanceColor = "green";
+  }
+}
+
+function calculateCategoryExpenses(categoryName) {
+  let categoryTotal = 0;
+  for (const entry of expenseEntries) {
+    if (entry[0] === categoryName) {
+      categoryTotal += entry[1];
+    }
+  }
+  return categoryTotal;
+}
